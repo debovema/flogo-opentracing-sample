45b086458a0952036584dc2fa955375419bc5c3f
Mathieu Debove <debovema@users.noreply.github.com>
2018-10-03 11:26:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# flogo-opentracing-sample
---
+++ src/flogo-opentracing-sample/imports.go
@@ -0,0 +1,8 @@
+package main
+
+import (
+	_ "github.com/TIBCOSoftware/flogo-contrib/action/flow"
+	_ "github.com/TIBCOSoftware/flogo-contrib/activity/log"
+	_ "github.com/TIBCOSoftware/flogo-contrib/trigger/rest"
+	_ "github.com/debovema/flogo-contrib-models/opentracing"
+)
