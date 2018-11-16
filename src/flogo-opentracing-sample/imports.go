43a89c5db73ac041724b9a018ff221f5baa244ea
Mathieu Debove <mathieu.debove@sqits.net>
2018-11-16 17:23:04 +0100

    Update sample to v0.5.7 of Flogo

--- src/flogo-opentracing-sample/imports.go
+++ src/flogo-opentracing-sample/imports.go
@@ -4,6 +4,6 @@ import (
 	_ "github.com/TIBCOSoftware/flogo-contrib/action/flow"
 	_ "github.com/TIBCOSoftware/flogo-contrib/activity/log"
 	_ "github.com/TIBCOSoftware/flogo-contrib/trigger/rest"
-	_ "github.com/debovema/flogo-opentracing-listener"
+	_ "github.com/square-it/flogo-opentracing-listener"
 	_ "github.com/square-it/flogo-contrib-activities/sleep"
 )
